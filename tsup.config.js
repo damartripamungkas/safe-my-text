bb8a964c9fe23d4d877e21d78b96195396c9f0e4
damartripamungkas <damartripamungkas@github.com>
2024-07-07 05:53:26 +0700

    feat: remove iife format bundle

--- tsup.config.js
+++ tsup.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from "tsup"
 export default defineConfig({
   outDir: `./dist/src`,
   entry: [`./src/index.ts`],
-  format: [`cjs`, `esm`, `iife`],
+  format: [`cjs`, `esm`],
   cjsInterop: true,
   dts: true,
   clean: true,
